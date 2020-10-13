3d381b622cf57b6d5b3b6a2c4ed8298d963428d8
canji53 <canji.preengineer@gmail.com>
2020-10-13 13:47:20 +0900

    :+1: corsのoriginを限定化

--- content-list/read/app.js
+++ content-list/read/app.js
@@ -167,7 +167,7 @@ module.exports.readContentList = async (event, context) => {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*.localing.ml'
       },
       body: JSON.stringify({
         contentList: queryResponse.contentList,
@@ -181,7 +181,7 @@ module.exports.readContentList = async (event, context) => {
       statusCode: 400,
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*.localing.ml'
       },
       body: JSON.stringify({
         message: error.message
--- media-list/read/app.js
+++ media-list/read/app.js
@@ -80,7 +80,7 @@ module.exports.readMediaList = async (event) => {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*.localing.ml'
       },
       body: JSON.stringify({
         mediaList: queryResponse.mediaList
@@ -94,7 +94,7 @@ module.exports.readMediaList = async (event) => {
       statusCode: 400,
       headers: {
         'Content-Type': 'application/json; charset=utf-8',
-        'Access-Control-Allow-Origin': '*'
+        'Access-Control-Allow-Origin': '*.localing.ml'
       },
       body: JSON.stringify({
         message: error.message
